549e391d523f3e40ebab7aa59abbe295e87093f8
Wyatt Olson <wyatt.olson@gmail.com>
2011-02-01 16:36:27 -0700

    Work in progress: adding RSS feed

--- src/ca/digitalcave/moss/jsp/photos/Common.java
+++ src/ca/digitalcave/moss/jsp/photos/Common.java
@@ -1,10 +1,19 @@
 package ca.digitalcave.moss.jsp.photos;
 
+import java.io.InputStream;
+import java.util.Date;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.PageContext;
+
+import com.drew.imaging.jpeg.JpegMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifDirectory;
+import com.drew.metadata.iptc.IptcDirectory;
 
 public class Common {
 	public static String getFullQualityUrlFromFile(ServletContext servletContext, String path){
@@ -99,4 +108,41 @@ public class Common {
 
 		return type;
 	}
+	
+	public static ImageMetadata getImageMetadata(InputStream image){
+		String title = null, caption = null;
+		Date captureDate = null;
+		try {			
+			Metadata metadata = JpegMetadataReader.readMetadata(image);
+			Directory iptcDirectory = metadata.getDirectory(IptcDirectory.class);
+			Directory exifDirectory = metadata.getDirectory(ExifDirectory.class);
+			
+			//OBJ_NAME is the field that Lightroom's 'Title' attribute is saved to.
+			if (iptcDirectory.containsTag(IptcDirectory.TAG_OBJECT_NAME)){
+				title = iptcDirectory.getString(IptcDirectory.TAG_OBJECT_NAME);
+			}
+			if (iptcDirectory.containsTag(IptcDirectory.TAG_CAPTION)){
+				caption = iptcDirectory.getString(IptcDirectory.TAG_CAPTION);
+			}
+			if (exifDirectory.containsTag(ExifDirectory.TAG_DATETIME_ORIGINAL)){
+				captureDate = exifDirectory.getDate(ExifDirectory.TAG_DATETIME_ORIGINAL);
+			}
+		}
+		catch (Throwable t) {}
+		
+		return new ImageMetadata(title, caption, captureDate);
+	}
+	
+	
+	public static String escapeHtml(String s){
+		if (s == null)
+			return null;
+		return s
+		.replaceAll("'", "&apos;")
+		.replaceAll("\"", "&quot;")
+		.replaceAll("<", "&lt;")
+		.replaceAll(">", "&gt;")
+		.replaceAll("&", "&amp;")
+		.replaceAll("[\r\n]", "<br/>");
+	}	
 }
--- src/ca/digitalcave/moss/jsp/photos/RssTag.java
+++ src/ca/digitalcave/moss/jsp/photos/RssTag.java
@@ -1,9 +1,11 @@
 package ca.digitalcave.moss.jsp.photos;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.jsp.JspException;
@@ -11,7 +13,13 @@ import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.Tag;
 
-public class GalleryTag implements Tag {
+import com.drew.imaging.jpeg.JpegMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifDirectory;
+import com.drew.metadata.iptc.IptcDirectory;
+
+public class RssTag implements Tag {
 	private PageContext pageContext = null;
 	private Tag parent = null;
 
@@ -19,23 +27,15 @@ public class GalleryTag implements Tag {
 	
 	private String packageName = ".";
 	
-	private int thumbSize = 130; //If thumbSize is 0 we will use full images (scaled in galleria) for thumbs.  This allows pre-loading an entire page for quicker access; however it will slow down the initial rendering for large pages.
-	private int fullSize = 800;
-	private int thumbQuality = 75;
-	private int fullQuality = 85;
+	private int size = 600;
+	private int quality = 85;
 	
-	private boolean center = true;
 	private String order = "sorted";
-	
-	private boolean slideshow = false; //Is slideshow enabled?
-	private int slideshowDelay = 5000; //How long to wait in between slideshow slides (in millis); only used if slideshow is enabled
-	private boolean slideshowAllowOverride = false; //Can the user stop the slideshow?  (i.e. is the carosel there?)
 	
 	private boolean showTitle = false;
 	private boolean showDate = false;
 	private boolean showCaption = false;
 	private boolean showFilename = false;
-	private boolean showFullQualityDownload = false;
 
 	private String matchRegex = "^.*png$|^.*jpg$|^.*jpeg$|^.*bmp$|^.*png$|^.*gif$";
 	private String excludeRegex = ".*/\\.[^/]*"; //Hide all dot files
@@ -202,30 +202,50 @@ public class GalleryTag implements Tag {
 					}
 					pageContext.getOut().println("<img src='" + Common.getUrlFromFile(pageContext.getServletContext(), imagePath, (getThumbSize() == 0 ? getFullSize() : getThumbSize()), (getThumbSize() == 0 ? getFullQuality() : getThumbQuality())) + "'");
 
-					ImageMetadata imageMetadata = null;					
+					String title = null, caption = null;
+					Date date = null;
+					
 					if (isShowTitle() || isShowCaption()){
-						//Try to load metadata; possibly used for title / caption
-						imageMetadata = Common.getImageMetadata(pageContext.getServletContext().getResourceAsStream(imagePath.replaceAll("%20", " ")));
+						try {
+							//Try to load metadata; possibly used for title / caption
+							InputStream is = pageContext.getServletContext().getResourceAsStream(imagePath.replaceAll("%20", " "));
+							
+							Metadata metadata = JpegMetadataReader.readMetadata(is);
+							Directory iptcDirectory = metadata.getDirectory(IptcDirectory.class);
+							Directory exifDirectory = metadata.getDirectory(ExifDirectory.class);
+							
+							//OBJ_NAME is the field that Lightroom's 'Title' attribute is saved to.
+							if (iptcDirectory.containsTag(IptcDirectory.TAG_OBJECT_NAME)){
+								title = iptcDirectory.getString(IptcDirectory.TAG_OBJECT_NAME);
+							}
+							if (iptcDirectory.containsTag(IptcDirectory.TAG_CAPTION)){
+								caption = iptcDirectory.getString(IptcDirectory.TAG_CAPTION);
+							}
+							if (exifDirectory.containsTag(ExifDirectory.TAG_DATETIME_ORIGINAL)){
+								date = exifDirectory.getDate(ExifDirectory.TAG_DATETIME_ORIGINAL);
+							}
+						}
+						catch (Throwable t) {}
 					}
 					
 					StringBuilder info = new StringBuilder();
-					if (isShowTitle() && imageMetadata.getTitle() != null){
+					if (isShowTitle() && title != null){
 						pageContext.getOut().println(" title='");
-						pageContext.getOut().println(Common.escapeHtml(imageMetadata.getTitle())); 
+						pageContext.getOut().println(escapeHtml(title)); 
 						pageContext.getOut().println("'");
 					}
 					
-					if (isShowCaption() && imageMetadata.getCaption() != null){
-						info.append("<p>").append(Common.escapeHtml(imageMetadata.getCaption())).append("</p>");
+					if (isShowCaption() && caption != null){
+						info.append("<p>").append(escapeHtml(caption)).append("</p>");
 					}
 					
-					if (isShowDate() && imageMetadata.getCaptureDate() != null){
-						info.append("<p>").append(new SimpleDateFormat("yyyy-MM-dd").format(imageMetadata.getCaptureDate())).append("</p>");
+					if (isShowDate() && date != null){
+						info.append("<p>").append(new SimpleDateFormat("yyyy-MM-dd").format(date)).append("</p>");
 					}					
 					
 					if (isShowFilename()){
 						final String filename = imagePath.replaceAll("^/.*/", "");
-						info.append("<p>").append(Common.escapeHtml(filename)).append("</p>");
+						info.append("<p>").append(escapeHtml(filename)).append("</p>");
 					}
 					
 					if (info.length() > 0){
@@ -289,5 +309,17 @@ public class GalleryTag implements Tag {
 	public void release() {
 		pageContext = null;
 		parent = null;
+	}
+	
+	private String escapeHtml(String s){
+		if (s == null)
+			return null;
+		return s
+		.replaceAll("'", "&apos;")
+		.replaceAll("\"", "&quot;")
+		.replaceAll("<", "&lt;")
+		.replaceAll(">", "&gt;")
+		.replaceAll("&", "&amp;")
+		.replaceAll("[\r\n]", "<br/>");
 	}
 }
